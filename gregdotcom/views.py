7fe7ac912d7c91da36c47a7f5fe3ae56a89ff90a
gregorydiaz <gr3gdiaz@gmail.com>
2021-02-23 21:43:51 -0800

    Migrations and project note updates

--- gregdotcom/views.py
+++ gregdotcom/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging
+
 
 def homePageView(request):
     return HttpResponse("""<div style='width:100%;height:100%; background-color:#ff304f;color:purple;font-size:52; font-weight:bold;'>Hello, World!<div>""")
+
+
+    
